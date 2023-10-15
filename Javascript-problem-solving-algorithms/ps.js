4e8e54a60533cd03d9aaa5dbc79597d36cc444fd
Johan Swan <jswannieman@gmail.com>
2023-10-16 08:55:29 +1300

    Commit ps Javascript Component

--- Javascript-problem-solving-algorithms/ps.js
+++ Javascript-problem-solving-algorithms/ps.js
@@ -1,22 +1,11 @@
-const startThatClock = () => {
-  let date = new Date();
-  let time = date.toLocaleTimeString();
-  if (time.substring(0, 1) > 1) {
-    time.replace(/^/, "0");
+function factorialMethod1(value) {
+  let result = value;
+  if (value === 0 || value === 1) return 1;
+  while (value > 1) {
+    value--;
+    result *= value;
   }
-  document.getElementById("p1").innerHTML = time.substring(0, 2);
-  document.getElementById("p2").innerHTML = time.substring(2, 3);
-  document.getElementById("p3").innerHTML = time.substring(3, 5);
-  document.getElementById("p4").innerHTML = time.substring(5, 6);
-  document.getElementById("p5").innerHTML = time.substring(6, 8);
-};
+  return result;
+}
 
-let timeInterval;
-
-const startTimeClock = () => {
-  timeInterval = setInterval(startThatClock, 1000);
-};
-
-const stopTimeClock = () => {
-  clearInterval(timeInterval);
-};
+alert(factorialMethod1(4));
