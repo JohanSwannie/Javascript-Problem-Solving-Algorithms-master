ef9c4a8549dd1171fdd9570d4a83bc106d81bbd8
Johan Swan <jswannieman@gmail.com>
2023-10-16 11:18:47 +1300

    Commit ps Javascript Component

--- Javascript-problem-solving-algorithms/ps.js
+++ Javascript-problem-solving-algorithms/ps.js
@@ -2,6 +2,8 @@ const findTheLongestWordInString2 = (string) => {
   let longestWord2 = string.split(" ").sort((a, b) => b.length - a.length);
   return `The Longest Word in the String is ${longestWord2[0]} and is ${longestWord2[0].length} characters long`;
 };
+
 let string1 =
   "Hallo almal wat graag wil leer om te sien hoe werk fibonacci nommers";
+
 alert(findTheLongestWordInString2(string1));
