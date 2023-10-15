a4b8b51f3dd53c7e6ee00c5b56dbce2ec1311a46
Johan Swan <jswannieman@gmail.com>
2023-10-16 11:00:56 +1300

    Commit ps Javascript Component

--- Javascript-problem-solving-algorithms/ps.js
+++ Javascript-problem-solving-algorithms/ps.js
@@ -1,8 +1,13 @@
-const factorialMethod3 = (value) => {
-  if (value === 0 || value === 1) {
-    return 1;
-  }
-  return value * factorialMethod3(value - 1);
-};
+function checkIfPalindrome(string) {
+  let regex1 = /[\W]/g;
+  let lowercaseString = string.toLowerCase().replace(regex1, "");
+  let reverseString = lowercaseString.split("").reverse().join("");
+  return reverseString === lowercaseString;
+}
 
-alert(factorialMethod3(4));
+const string1 = "Sir, I demand, I am a maid named Iris";
+const string2 = "Sit on a potato pan, Otis";
+const string3 = "Go hang a salami, I'm a lasagna hog";
+const string4 = "Madam in Eden, I'm Adam";
+
+alert(checkIfPalindrome(string1));
