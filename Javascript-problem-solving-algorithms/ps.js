3d88dad47f6aec11ea85dde515a37ce89147b186
Johan Swan <jswannieman@gmail.com>
2023-10-16 14:07:06 +1300

    Commit ps Javascript Component

--- Javascript-problem-solving-algorithms/ps.js
+++ Javascript-problem-solving-algorithms/ps.js
@@ -1,9 +1,13 @@
-const findTheLongestWordInString2 = (string) => {
-  let longestWord2 = string.split(" ").sort((a, b) => b.length - a.length);
-  return `The Longest Word in the String is ${longestWord2[0]} and is ${longestWord2[0].length} characters long`;
-};
+function largestNumbersInArrays2(arr) {
+  return arr.map((pop) => pop.filter((amt) => amt > 100));
+}
 
-let string1 =
-  "Hallo almal wat graag wil leer om te sien hoe werk fibonacci nommers";
+const array11 = [
+  [71, 167, 202, 183, 16, 12],
+  [191, 37, 7, 28, 38, 20, 18],
+  [116, 41, 26, 53, 91, 16, 43],
+  [21, 50, 111, 30, 49, 123],
+  [41, 18, 401, 114, 22, 95],
+];
 
-alert(findTheLongestWordInString2(string1));
+alert(largestNumbersInArrays2(array11));
