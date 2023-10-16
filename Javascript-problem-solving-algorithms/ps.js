4f617f8810f4fa7c731538afec5441ac7537a135
Johan Swan <jswannieman@gmail.com>
2023-10-17 08:43:42 +1300

    Commit ps Javascript Component

--- Javascript-problem-solving-algorithms/ps.js
+++ Javascript-problem-solving-algorithms/ps.js
@@ -1,7 +1,6 @@
 function wordToNumericValue(word) {
   let numValue = 0;
   for (var i = 0; i < word.length; i++) {
-    alert(word[i]);
     numValue += parseInt(word[i], 36) - 9;
   }
   return numValue;
