a2e93b2cf6e491e8c0e64d175fd9f28f7082ab05
Johan Swan <jswannieman@gmail.com>
2023-10-16 18:32:49 +1300

    Commit ps Javascript Component

--- Javascript-problem-solving-algorithms/ps.js
+++ Javascript-problem-solving-algorithms/ps.js
@@ -1,19 +1,12 @@
-function largestNumbersInArrays3(array) {
-  return array.map((successiveArray2) =>
-    Math.max.apply(null, successiveArray2)
-  );
+function wordToNumericValue(word) {
+  let numValue = 0;
+  for (var i = 0; i < word.length; i++) {
+    alert(word[i]);
+    numValue += parseInt(word[i], 36) - 9;
+  }
+  return numValue;
 }
 
-const array12 = [
-  [4, 7, 20, 1, 9, 31, 6, 11, 8],
-  [23, 45, 33, 20, 41, 19, 25],
-  [37, 59, 65, 35, 14, 22, 88],
-  [10, 20, 60, 30, 70, 40],
-  [38, 27, 29, 47, 13, 12, 36],
-];
+let word = "Fantastic";
 
-alert(
-  `Largest of all numbers is ${
-    largestNumbersInArrays3(array12).sort((a, b) => b - a)[0]
-  }`
-);
+alert(wordToNumericValue(word));
