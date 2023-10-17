4d344059459e0e965513a737f0d24bebf67301b6
Johan Swan <jswannieman@gmail.com>
2023-10-18 08:34:25 +1300

    Commit ps Javascript Component

--- Javascript-problem-solving-algorithms/ps.js
+++ Javascript-problem-solving-algorithms/ps.js
@@ -1,24 +1,28 @@
-function removeSmallest(numbers) {
-  let newArray = [];
-  if (numbers.length === 0) {
-    return newArray;
-  }
-  let smallestIndex = 0;
-  let smallestNumber = numbers[0];
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] < smallestNumber) {
-      smallestNumber = numbers[i];
-      smallestIndex = i;
+const binarySearch = (arr, value) => {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start < end) {
+    let middle = Math.floor((start + end) / 2);
+    if (value === arr[start]) {
+      return `The value ${value} is at index ${start}`;
+    }
+    if (value === arr[middle]) {
+      return `The value ${value} is at index ${middle}`;
+    }
+    if (value === arr[end]) {
+      return `The value ${value} is at index ${end}`;
+    }
+    if (value > arr[middle]) {
+      start = middle + 1;
+    } else {
+      end = middle - 1;
     }
   }
-  newArray = numbers
-    .slice(0, smallestIndex)
-    .concat(numbers.slice(smallestIndex + 1));
-  return newArray;
-}
+  return `The value ${value} is not found`;
+};
 
-alert(
-  `New array after first smallest number is removed, is : ${removeSmallest([
-    5, 9, 9, 2, 1, 3, 8, 1, 2, 7,
-  ])}`
-);
+let arr = [10, 44, 8, 19, 16, 10, 29, 22, 30, 14, 8, 20];
+
+arr.sort((a, b) => a - b);
+
+alert(binarySearch(arr, 29));
