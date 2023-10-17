b36a8e5f736bd1197870266578aacf830aa8e790
Johan Swan <jswannieman@gmail.com>
2023-10-17 16:26:11 +1300

    Commit ps Javascript Component

--- Javascript-problem-solving-algorithms/ps.js
+++ Javascript-problem-solving-algorithms/ps.js
@@ -1,15 +1,17 @@
-const findTheLongestWordInString3 = (string) => {
-  let longestWord3 = string
-    .split(" ")
-    .reduce(
-      (returnWord, currentWord) =>
-        currentWord.length > returnWord.length ? currentWord : returnWord,
-      ""
-    );
-  return `The longest word is ${longestWord3} and is ${longestWord3.length} characters long`;
-};
+let array = [17, 23, 15, 29, 3, 15, 19, 3, 15, 10, 7, 14, 3, 19];
 
-const string8 =
-  "Tommy eat healthy and consumes sufficient amounts of nutrients daily";
+let array2 = [];
 
-alert(findTheLongestWordInString3(string8));
+for (let i = 0; i < array.length; i++) {
+  if (array2.indexOf(array[i]) === -1) {
+    array2.push(array[i]);
+  }
+}
+
+array2.sort((a, b) => a - b);
+
+alert(array2);
+
+let numbs = [12, 18, 17, 21, 10, 26, 35, 7, 18];
+
+alert(numbs.findIndex((num) => num === numbs[numbs.length - 6]));
