306234be41a8f510bd537291bcfe93aecb7793ee
Johan Swan <jswannieman@gmail.com>
2023-10-18 09:31:17 +1300

    Update ps Javascript Component

--- Javascript-problem-solving-algorithms/ps.js
+++ Javascript-problem-solving-algorithms/ps.js
@@ -1,28 +1,14 @@
-const binarySearch = (arr, value) => {
-  let start = 0;
-  let end = arr.length - 1;
-  while (start < end) {
-    let middle = Math.floor((start + end) / 2);
-    if (value === arr[start]) {
-      return `The value ${value} is at index ${start}`;
-    }
-    if (value === arr[middle]) {
-      return `The value ${value} is at index ${middle}`;
-    }
-    if (value === arr[end]) {
-      return `The value ${value} is at index ${end}`;
-    }
-    if (value > arr[middle]) {
-      start = middle + 1;
-    } else {
-      end = middle - 1;
-    }
+const fibonacciMethod = (num) => {
+  let x = 1;
+  let y = 0;
+  let temp = 0;
+  while (num > 0) {
+    temp = x;
+    x = x + y;
+    y = temp;
+    num--;
   }
-  return `The value ${value} is not found`;
+  return y;
 };
 
-let arr = [10, 44, 8, 19, 16, 10, 29, 22, 30, 14, 8, 20];
-
-arr.sort((a, b) => a - b);
-
-alert(binarySearch(arr, 29));
+alert(fibonacciMethod(5));
