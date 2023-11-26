2a214bc90483a8f14a142b695f2dd7e8f2456c06
Johan Swan <jswannieman@gmail.com>
2023-11-27 12:22:33 +1300

    update ps JS Component

--- Javascript-problem-solving-algorithms/ps.js
+++ Javascript-problem-solving-algorithms/ps.js
@@ -3,6 +3,17 @@ const findElement = (array, element) =>
 
 let array = [19, 20, true, "Koos", 29, false, "Jannie", "Mary"];
 
-alert(
-  `The name "Koos" is found in the array at index ${findElement(array, "Koos")}`
-);
+const container = document.querySelector(".container");
+let n = 1;
+let p = "";
+
+array.map((element) => {
+  const pippie = document.createElement("p");
+  p = `p${n}`;
+  pippie.setAttribute("id", p);
+  pippie.innerHTML = `The element [ ${element} ] is found in the array at index ${findElement(
+    array,
+    element
+  )}`;
+  container.appendChild(pippie);
+});
